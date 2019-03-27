1ccb02208de99623e193cd2485bbb001520f6dc4
Olena Ageyeva <eageeva@hotmail.com>
2019-03-27 00:51:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time=Time.now
-
+    resp.write "#{}{Time.zone)}"
     if time.hour<12
       resp.write "Good Morning! Time now: #{time}"
     else
